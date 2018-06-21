3d4155b6a26b19f08c47d3898848ac8f74035644
J. Del Angel <jadelangel18@gmail.com>
2018-06-21 10:40:39 -0500

    removed jquery/ eventlistener for keypress's works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,30 @@
 
 
+//defining variables
+var wins = 0;
+var losses = 0;
+var guessesleft = 5;
+var guesses = [];
 
 
+//creating store of alaphebits for computer to reference
 
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaLength = alpha.length;
+var compLetter = Math.floor(Math.random() * 25); 
+
+
+//looking for key stroke and outputting it as console.log
+var userinput;
+
+document.addEventListener('keypress', (event) => {
+    var keyName = event.key;
+    userinput = keyName.toLowerCase();
+    console.log(userinput);
+  });
+
+
+//
 
 
 
